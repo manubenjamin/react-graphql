f490d838716410e9877528e8b4407da755af5fd7
Manu Benjamin <manu.benjamin@gmail.com>
2019-04-08 18:01:15 +0530

    Added back button

--- components/SongList.js
+++ components/SongList.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import gql from 'graphql-tag';
 import { graphql } from 'react-apollo';
+import { Link } from 'react-router';
 
 class SongList extends Component {
     renderSongs() {
@@ -21,6 +22,10 @@ class SongList extends Component {
                 <ul className="collection">
                     {this.renderSongs()}
                 </ul>
+                <Link to="/songs/new"
+                    className="btn-floating btn-large red right">
+                    <i className="material-icons">add</i>
+                </Link>
             </div>
         )
     }
