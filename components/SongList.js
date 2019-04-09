cef9954bb59a9fba16614e09aed26288c0aed51c
Manu Benjamin <manu.benjamin@gmail.com>
2019-04-09 14:03:47 +0530

    Delete song mutation

--- components/SongList.js
+++ components/SongList.js
@@ -5,10 +5,22 @@ import { Link } from 'react-router';
 import query from '../queries/fetchSongs';
 
 class SongList extends Component {
+    songDelete(id) {
+        this.props.mutate({
+            variables: {
+                id: id
+            },
+            refetchQueries: [{query: query}]
+        })
+    }
+
     renderSongs() {
         return this.props.data.songs.map(song => {
             return (
-                <li key={song.id}  className="collection-item">{song.title}</li>
+                <li key={song.id}  className="collection-item">
+                    {song.title}
+                    <i className="material-icons" onClick={() => this.songDelete(song.id) }>delete</i>
+                </li>
             )
         })
     }
@@ -32,4 +44,14 @@ class SongList extends Component {
     }
 }
 
-export default graphql(query)(SongList);
+const mutation = gql`
+    mutation DeleteSong($id: ID) {
+        deleteSong(id: $id) {
+            id
+        }
+    }
+`;
+
+export default graphql(mutation) (
+    graphql(query)(SongList)
+);
